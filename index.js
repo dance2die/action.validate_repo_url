7840d82654ea1cbac61105859e86551db2ccc100
Sung M. Kim <sungmkim80@gmail.com>
2020-09-17 19:49:11 -0400

    patterns global

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const core = require("@actions/core");
 const glob = require("@actions/glob");
 
-const patterns = ["docs/**/*", "!node_modules", "!package*.json"];
+// const patterns = ["docs/**/*", "!node_modules", "!package*.json"];
+const patterns = ["**"];
 
 (async function () {
   const globber = await glob.create(patterns.join("\n"));
