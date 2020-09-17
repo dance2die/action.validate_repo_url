63e6fb07eb98b19cdfffdcf922c195d64c663da2
Sung M. Kim <sungmkim80@gmail.com>
2020-09-17 19:21:34 -0400

    uses github core to show message

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const globby = require("globby");
+const core = require("@actions/core")(
+  // https://repl.it/@dance2die/glob-promise-demo#index.js
 
-// https://repl.it/@dance2die/glob-promise-demo#index.js
-(async () => {
-  const paths = await globby(["docs/**/*", "!node_modules", "!package*.json"]);
-  console.log(paths);
-})();
+  async () => {
+    const paths = await globby([
+      "docs/**/*",
+      "!node_modules",
+      "!package*.json",
+    ]);
+    core.info(`paths ==> ${JSON.stringify(paths, null, 2)}`);
+  }
+)();
