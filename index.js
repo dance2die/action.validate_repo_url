bad046a05d1d63f9a9e7d91a2b12b13ffa8235d1
Sung M. Kim <sungmkim80@gmail.com>
2020-09-17 19:36:16 -0400

    fixed the require is not a function error by adding semicolon

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const globby = require("globby");
-const core = require("@actions/core")(
-  // https://repl.it/@dance2die/glob-promise-demo#index.js
+const core = require("@actions/core");
 
-  async () => {
-    const paths = await globby([
-      "docs/**/*",
-      "!node_modules",
-      "!package*.json",
-    ]);
-    core.info(`paths ==> ${JSON.stringify(paths, null, 2)}`);
-  }
-)();
+// https://repl.it/@dance2die/glob-promise-demo#index.js
+
+(async () => {
+  const paths = await globby(["docs/**/*", "!node_modules", "!package*.json"]);
+  core.info(`paths ==> ${JSON.stringify(paths, null, 2)}`);
+})();
