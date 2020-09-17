42159342d2c44a0b6077d7543e0ca98ae068e122
Sung M. Kim <sungmkim80@gmail.com>
2020-09-17 19:47:59 -0400

    woops. forgot to wrap async functions

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const core = require("@actions/core");
 const glob = require("@actions/glob");
 
 const patterns = ["docs/**/*", "!node_modules", "!package*.json"];
-const globber = await glob.create(patterns.join("\n"));
-const files = await globber.glob();
 
-core.info(`files ==> ${JSON.stringify(files, null, 2)}`);
+(async function () {
+  const globber = await glob.create(patterns.join("\n"));
+  const files = await globber.glob();
+
+  core.info(`files ==> ${JSON.stringify(files, null, 2)}`);
+})();
 
 // const globby = require("globby");
 
