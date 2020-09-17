3318b4633951fb57af75d968763b36a401fa9db6
Sung M. Kim <sungmkim80@gmail.com>
2020-09-17 19:01:52 -0400

    Added a sample code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const globby = require("globby");
 
+// https://repl.it/@dance2die/glob-promise-demo#index.js
 (async () => {
   const paths = await globby(["docs/**/*", "!node_modules", "!package*.json"]);
   console.log(paths);
