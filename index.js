c1d3f40d6e2a3beaf9d9a0f628b2b0083e8fdd81
Sung M. Kim <sungmkim80@gmail.com>
2020-09-17 21:23:03 -0400

    changed pattern to docs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const core = require("@actions/core");
 const glob = require("@actions/glob");
 
-// const patterns = ["docs/**/*", "!node_modules", "!package*.json"];
-const patterns = ["**"];
+const patterns = ["docs/**/*", "!node_modules", "!package*.json"];
+// const patterns = ["**"];
 
 (async function () {
   const globber = await glob.create(patterns.join("\n"));
@@ -10,12 +10,3 @@ const patterns = ["**"];
 
   core.info(`files ==> ${JSON.stringify(files, null, 2)}`);
 })();
-
-// const globby = require("globby");
-
-// // https://repl.it/@dance2die/glob-promise-demo#index.js
-
-// (async () => {
-//     const paths = await globby(["docs/**/*", "!node_modules", "!package*.json"]);
-//     core.info(`paths ==> ${JSON.stringify(paths, null, 2)}`);
-// })();
