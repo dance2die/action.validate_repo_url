bbd20d70a5280bfb7e530a33fd9eee1ecb1521c3
Sung M. Kim <sungmkim80@gmail.com>
2020-09-17 21:29:51 -0400

    excludes directories

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const core = require("@actions/core");
 const glob = require("@actions/glob");
+const fs = require("fs").promises;
 
 const patterns = ["docs/**/*", "!node_modules", "!package*.json"];
 // const patterns = ["**"];
 
+const isFile = async (path) => {
+  const stat = await fs.lstat(path);
+  return stat.isFile();
+};
+
 (async function () {
   const globber = await glob.create(patterns.join("\n"));
-  const files = await globber.glob();
+  // exclude directories
+  const files = (await globber.glob()).filter(
+    async (path) => await isFile(path)
+  );
 
   core.info(`files ==> ${JSON.stringify(files, null, 2)}`);
 })();
