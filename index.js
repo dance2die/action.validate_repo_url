479670b9190c99600d87a7e724d78337a5d7bbac
Sung M. Kim <sungmkim80@gmail.com>
2020-09-17 22:43:38 -0400

    unable extract urls for some reason

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ const extractUrls = async (file) => {
   const text = await fs.readFileSync(file, "utf-8");
   const urls = [...getUrls(text)];
 
-  //   core.info(`${file} => ${text}`, JSON.stringify(text, null, 2));
-
-  core.info(`extractUrls==> ${file}`, JSON.stringify({ file, urls }, null, 2));
+  core.info(`extractUrls==> ${file}, ${JSON.stringify(urls, null, 2)}`);
   return urls;
 };
 
